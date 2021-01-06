954cb4bd4c0ebd31df3ab6301edf443992bdeb02
guduri.sriram <guduri.sriram@inmobi.com>
2021-01-06 09:19:01 +0530

    complete regex

--- python3/regex_matching.py
+++ python3/regex_matching.py
@@ -1,21 +1,19 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        if not s or len(s) == 0:
-            return p == '.' or p == '.*'
-        if not p or len(p) == 0:
-            return False
+        memo = dict()
 
-        slen = len(s)
-        plen = len(p)
+        def dp(i: int, j: int) -> bool:
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if j == len(p):
+                memo[(i, j)] = i == len(s)
+                return memo[(i, j)]
+            first_match = i < len(s) and p[j] in [s[i], '.']
+            if j <= len(p)-2 and p[j+1] == '*':
+                memo[(i, j)] = (first_match and dp(i+1, j)) or dp(i, j+2)
+                return memo[(i, j)]
+            else:
+                memo[(i, j)] = first_match and dp(i+1, j+1)
+                return memo[(i, j)]
 
-        def recursive_match(sc: int, pc: int) -> bool:
-            if sc >= slen:
-                return pc >= plen or (pc == plen-2 and p[pc+1] == '*')
-            if pc >= plen:
-                return False
-            first_match = p[pc] in [s[sc], '.']
-            if pc+1 < plen and p[pc+1] == '*':
-                return (first_match and recursive_match(sc+1, pc)) or recursive_match(sc, pc+2)
-            return first_match and recursive_match(sc+1, pc+1)
-
-        return recursive_match(0, 0)
+        return dp(0, 0)
