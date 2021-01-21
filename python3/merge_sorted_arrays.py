c2010e74a5ccc2c6995072f9422af7730327a9f5
guduri.sriram <guduri.sriram@inmobi.com>
2021-01-21 08:21:14 +0530

    merge sorted arrays in place

---
+++ python3/merge_sorted_arrays.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        last = m + n - 1
+        m, n = m-1, n-1
+        while m >= 0 and n>= 0:
+            if nums1[m] > nums1[n]:
+                nums1[last] = nums1[m]
+                m = m-1
+            else:
+                nums1[last] = nums2[n]
+                n = n - 1
+            last = last-1
+
+        while n >= 0:
+            nums1[last] = nums2[n]
+            n = n - 1
+            last = last-1
