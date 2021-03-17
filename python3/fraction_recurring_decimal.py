913c2e70c12902e7222dfb2357981a838480cf0c
guduri.sriram <guduri.sriram@inmobi.com>
2021-03-17 09:00:05 +0530

    fraction to recurring decimals

---
+++ python3/fraction_recurring_decimal.py
@@ -0,0 +1,21 @@
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        res = ''
+        if numerator * denominator < 0:
+            res += '-'
+        numerator = abs(numerator)
+        denominator = abs(denominator)
+        q = numerator//denominator
+        res += str(q)
+        r = numerator%denominator
+        if r > 0:
+            res += '.'
+        r_idx = {}
+        while r:
+            if r in r_idx:
+                res = res[:r_idx[r]] + '(' + res[r_idx[r]:] + ')'
+                break
+            r_idx[r] = len(res)
+            res += str((r*10)//denominator)
+            r = (r*10)%denominator
+        return res
